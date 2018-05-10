401bee0d0f1947bec1ceebad28a8df7feee8b476
silvestrevivo <silvestrevivomillan@gmail.com>
2018-05-10 11:12:26 +0200

    first approaching to routing requests

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = http.createServer((req, res) => {
 
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
-  console.log('Query parameters as object', queryStringObject)
+  // console.log('Query parameters as object', queryStringObject)
   //http://localhost:3000/status?title=ElQuijote&author=Cervantes
   //queryStringObject => { title: 'ElQuijote', author: 'Cervantes' }
 
@@ -34,24 +34,71 @@ const server = http.createServer((req, res) => {
 
   // Get the payload, if any
   const decoder = new StringDecoder('utf-8');
-  const buffer = '';
+  let buffer = '';
   req.on('data', (data) => {
     buffer += decoder.write(data);
   });
 
-  req.on('end', () => {
+  req.on('end', () => { // this happens when the request is ended
     buffer += decoder.end();
 
-    // Response from the server to the screen when the buffer is ended
-    res.end(`Hello World! This is printed on screen.
-    You are connected to the port localhost://3000${trimmedPath ? `/${trimmedPath}` : ''}`);
+    // Choose the handler this request should to go
+    // If one is not found use the notFound handler
+    const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-    console.log('Request recieved with this payload:', buffer);
+    // Construct the data object to send to the handler
+    const data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    }
 
+    // Route the request to the handler specificied in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the status code called back by the hanaler, or default to 200
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+      // Use the payload called back by the handler, or default to an empty object
+      payload = typeof (payload) == 'object' ? payload : {};
+
+      // Convert the paylad to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.writeHead(statusCode)
+
+      // Response from the server to the screen when the buffer is ended
+      res.end(`Hello World! This is printed on screen.
+      You are connected to the port localhost://3000${trimmedPath ? `/${trimmedPath}` : ''}.
+      Request recieved with this payloadString: ${payloadString}.`);
+
+      // Log the request path
+      console.log('Returning this response:', statusCode, payloadString);
+    })
   });
+
 });
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, () => {
   console.log('The server is listening on port 3000 now')
 });
+
+// Defining handlers
+const handlers = {
+  sample: (data, callback) => {
+    // Callback an http status code, and a payload object
+    callback(406, { 'name': 'sample handler' })
+  },
+  notFound: (data, callback) => {
+    callback(404)
+  }
+};
+
+// Defining a request router
+const router = {
+  'sample': handlers.sample,
+  'notFound': handlers.notFound
+};
